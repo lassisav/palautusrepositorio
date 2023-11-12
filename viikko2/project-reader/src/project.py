3d3380ae83a4c40850078375c08d8aa0db7de6de
Lassi Savolainen <lassi.savolainen@helsinki.fi>
2023-11-12 02:10:20 +0200

    Tehtävän 2 hiottu ratkaisu.

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,34 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, dependencies, dev_dependencies, authors):
         self.name = name
         self.description = description
+        self.license = license
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
+        self.authors = authors
 
     def _stringify_dependencies(self, dependencies):
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
+        auth = ""
+        for a in self.authors:
+            auth += " - " + a + "\n"
+        dep = ""
+        for b in self.dependencies:
+            dep += " - " + b + "\n"
+        dd = ""
+        for c in self.dev_dependencies:
+            dd += "\n - " + c
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license or '-'}"
+            f"\n"
+            f"\nAuthors:"
+            f"\n{auth or '-'}"
+            f"\nDependencies:"
+            f"\n{dep or '-'}"
+            f"\nDevelopment dependencies:"
+            f"{dd or '-'}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -9,13 +9,10 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        #print(content)
 
-        #print("väli")
-
+        #dict raakadatalle, ja pienenmpi dict tarpeelliselle datalle
         x = toml.loads(content)
         n = x['tool']['poetry']
-        #print(x)
-        #print("väli")
+
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(n['name'], n['description'], n['dependencies'], n['group']['dev']['dependencies'])
+        return Project(n['name'], n['description'], n['license'], n['dependencies'], n['group']['dev']['dependencies'], n['authors'])
