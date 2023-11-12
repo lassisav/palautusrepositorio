3251db8332bfe3771e2f3eedc83c59f656a23651
Lassi Savolainen <lassi.savolainen@helsinki.fi>
2023-11-13 01:59:34 +0200

    Tehtiin tehtävä 10.

---
+++ viikko2/nhl-reader/src/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.nationality = dict['nationality']
+    
+    def __str__(self):
+        return self.name + " team " + self.team + "  goals " + str(self.goals) + " assists " + str(self.assists)
+    
+    def getNat(self):
+        return self.nationality
