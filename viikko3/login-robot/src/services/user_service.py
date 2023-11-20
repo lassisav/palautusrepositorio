f61f4485584021a37afb7352b88443e255e96e7d
lassisav <lassi.savolainen@helsinki.fi>
2023-11-20 18:33:08 +0200

    Tehtävä 3 loppuun.

--- viikko3/login-robot/src/services/user_service.py
+++ viikko3/login-robot/src/services/user_service.py
@@ -1,5 +1,5 @@
 from entities.user import User
-
+import re
 
 class UserInputError(Exception):
     pass
@@ -18,7 +18,7 @@ class UserService:
             raise UserInputError("Username and password are required")
 
         user = self._user_repository.find_by_username(username)
-
+        
         if not user or user.password != password:
             raise AuthenticationError("Invalid username or password")
 
@@ -36,5 +36,24 @@ class UserService:
     def validate(self, username, password):
         if not username or not password:
             raise UserInputError("Username and password are required")
+        ### Too Short Username
+        if len(username) < 3:
+            raise UserInputError("Username too short")
+        
+        ### Username long enough but invalid
+        if not re.search("^[a-z]{3,}$", username):
+            raise UserInputError("Invalid username")
 
+        ### Password too short
+        if len(password) < 8:
+            raise UserInputError("Password too short")
+
+        ### Username taken
+        user  = self._user_repository.find_by_username(username)
+        if user != None:
+            raise AuthenticationError("Username already taken")
+        
+        ### Password only letters
+        if re.search("^[a-z]+$", password):
+            raise UserInputError("Password cannot contain only letters")
         # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
